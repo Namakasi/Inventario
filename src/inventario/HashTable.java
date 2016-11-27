15dcb95b3610bf1e6101d6785e2db4a7f7fe0eb6
Namakasi <poshino_666@hotmail.com>
2016-11-26 17:15:48 -0700

    stemen c entubó k elminiar por k zy

--- src/inventario/HashTable.java
+++ src/inventario/HashTable.java
@@ -10,18 +10,26 @@ package inventario;
  * @author Namakasi
  */
 public class HashTable {
-    private int m[] = new int[100];
+    private int M[] = new int[100];
     
     public HashTable(){
         
     }
     
-    private int hash(int k){//nomas pa calarlo el int
-        int key=0;
-        key = (k & 0x7fffffff)%m.length; //Operacion de modulo ya que esta nos generara un residuo,
+    public int hash(int key){
+        key = (key & 0x7fffffff)%M.length; //Operacion de modulo ya que esta nos generara un residuo,
         //0x7fffffff --> esta operacion fuerza a K a ser positivo numero maximo de enteros utilizados en java y otros lenguajes como c, c++ 0111-> 7 1111-> f 
         //El primer bit es el signo del numero 0= positivo 1 = mantendria el negativo
         //Es una alternativa a no usar math.abs();
         return key;
+    }
+    
+    public void insertar(Producto key, int valor){
+        
+    }
+    
+    public void search(int key){
+        int i = M[key];
+        
     }
 }
